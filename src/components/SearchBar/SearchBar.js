1ab94be1d590052fccc500ea594f6687d221818f
j <123@gmail.com>
2020-09-28 19:24:43 -0700

    add files

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            searchTerm: ""
+        }
+
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
+    handleTermChange(event){
+        this.setState({ searchTerm: event.target.value });
+    }
+
+    search(){
+        this.props.onSearch(this.state.searchTerm);
+    }
+
+    render(){
+        return (
+            <div className="SearchBar">
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
+                <button onClick={this.search} className="SearchButton">SEARCH</button>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
+ 
